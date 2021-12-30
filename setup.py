a8092d8fa1e9f5062788a1a5dc3d359e3fc407db
Afanasy Argunov <afanasy@argunov.com>
2021-12-30 20:12:27 +0900

    0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sciberia",
-    version="0.1.2",
+    version="0.1.3",
     description="Sciberia helper libraries",
     long_description=README,
     long_description_content_type="text/markdown",
